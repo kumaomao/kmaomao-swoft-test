c4e42220cae293be9a6df9d95e9d118dca15e7a1
kumaomao <z1083271136@163.com>
2019-05-14 17:20:47 +0800

    add

---
+++ src/TestBase.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: k
+ * Date: 2019/5/14
+ * Time: 16:25
+ */
+namespace km\test;
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * Class TestBase
+ * @package km\test
+ * @Target("METHOD")
+ */
+class TestBase
+{
+
+    public function __construct($name)
+    {
+        var_dump('test');
+    }
+
+}
---
+++ src/TestController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: k
+ * Date: 2019/5/14
+ * Time: 16:29
+ */
+
+namespace km\test;
+
+use Swoft\Bean\Annotation\Around;
+use Swoft\Bean\Annotation\Aspect;
+use Swoft\Bean\Annotation\PointAnnotation;
+
+/**
+ * Class TestController
+ * @package km\test
+ * @Aspect()
+ * @PointAnnotation(
+ *     include={
+            TestBase::class
+ *     }
+ * )
+ */
+class TestController
+{
+
+    /**
+     * @Around()
+     */
+    public function around(ProceedingJoinPoint $proceedingJoinPoint){
+        var_dump($proceedingJoinPoint);
+    }
+}
